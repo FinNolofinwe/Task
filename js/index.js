ef1a59dcbcc4ac10b8102d87917b351d69812ccf
FinNolofinwe <nolofinwe1881@gmail.com>
2021-08-31 08:43:09 +0300

    feat: add correct form validation

--- js/index.js
+++ js/index.js
@@ -3,22 +3,29 @@ document.addEventListener('DOMContentLoaded', () => {
   const warning = document.querySelector('.form__warning');
   const error = document.querySelector('.form__error');
   const submit = document.querySelector('.form__submit');
-  const widget = document.querySelector('.form__widget');
   
-  var rubCheck = document.getElementById('currency-ru'),
-      currCheck = widget.querySelectorAll('input[type=checkbox]:checked');
-      ageCheck = document.querySelector('.form__age-check'),
-      adultWarn = document.querySelector('.form__age-text');
+  var ageCheck = document.querySelector('.form__age-check'),
+      adultWarn = document.querySelector('.form__age-text'),
+      rubCurr = document.getElementById('currency-ru');
+      currencyType = '';
 
   var verifyMail = false,
       verifyPassword = false;
+
+  // Отменяем отправку формы по нажатию на enter
+
+  submit.addEventListener('keydown', (e) => {
+    if (e.key = "Enter") {
+      e.preventDefault();
+    }
+  });
 
   // События полей формы
 
   form.email.onfocus = function() {
     this.classList.add('input-active');
     this.previousElementSibling.classList.add('placeholder-active');
-  }
+  };
 
   // Проверка email
 
@@ -28,7 +35,7 @@ document.addEventListener('DOMContentLoaded', () => {
       warning.style.display = "flex";
     } else if (this.value.length == 0) {
       this.previousElementSibling.classList.remove('placeholder-active');
-    } else  {
+    } else {
       this.style.borderColor ="#20A86B";
       warning.style.display = "none";
       verifyMail = true;
@@ -46,28 +53,50 @@ document.addEventListener('DOMContentLoaded', () => {
     if ( this.value.length === 0 || this.value.length < 4) {
       this.style.borderColor ="#E15433";
       error.style.display = "block";
+      this.focus();
     } else {
       this.style.borderColor ="#20A86B";
       error.style.display = "none";
+      this.blur();
       verifyPassword = true;
     };
-  }
+  };
   
-  // Проверяем возрастное ограничение
+  //Событие выюора валюты
+  var currencyButton = document.querySelectorAll('.form__widget-check');
+
+  currencyButton.forEach(i => {
+    i.onchange = function(e) {
+      currencyType = i.nextElementSibling.innerHTML;
+    }
+  });
 
   submit.addEventListener('click', (e) => {
-    if (!ageCheck.checked) {
-      e.preventDefault();
-      ageCheck.nextElementSibling.focus();
-      adultWarn.style.backgroundColor = "#E15433";
-      adultWarn.style.color = "#ffffff";
+
+    if (verifyMail) {
+      if (verifyPassword) {
+        // Проверяем возрастное ограничение
+        if (!ageCheck.checked) {
+          e.preventDefault();
+          ageCheck.nextElementSibling.focus();
+          adultWarn.style.backgroundColor = "#E15433";
+          adultWarn.style.color = "#ffffff";
+        } else {
+          ageCheck.nextElementSibling.blur();
+          adultWarn.style.backgroundColor = "initial";
+          adultWarn.style.color = "#7A8F99";
+        }
+        // Проверяем тип валюты перед отправкой
+        if (currencyType.length == 0) {
+          currencyType = 'P';
+          rubCurr.checked = true;
+        };
+        e.target.submit();
+      } else {
+        form.password.style.borderColor = '#E15433';
+      }
     } else {
-      ageCheck.nextElementSibling.blur();
-      adultWarn.style.backgroundColor = "initial";
-      adultWarn.style.color = "#7A8F99";
-    }
-    if (currCheck.length == 0) {
-      rubCheck.checked = true;
+      form.email.style.borderColor = '#E15433';
     }
   });
 
