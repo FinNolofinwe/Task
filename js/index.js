ee1f0a9981ed3c06ab4f4de2d7bcc965f0772a0d
FinNolofinwe <nolofinwe1881@gmail.com>
2021-08-31 08:45:17 +0300

    feat: add forEach polyfill

--- js/index.js
+++ js/index.js
@@ -1,3 +1,16 @@
+/**
+ * NodeList.prototype.forEach() polyfill
+ * https://developer.mozilla.org/en-US/docs/Web/API/NodeList/forEach#Polyfill
+ */
+if (window.NodeList && !NodeList.prototype.forEach) {
+	NodeList.prototype.forEach = function (callback, thisArg) {
+		thisArg = thisArg || window;
+		for (var i = 0; i < this.length; i++) {
+			callback.call(thisArg, this[i], i, this);
+		}
+	};
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   const form = document.forms[0];
   const warning = document.querySelector('.form__warning');
